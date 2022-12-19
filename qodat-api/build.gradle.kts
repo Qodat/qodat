06751663b8f1f4367fddaa5905ddb0c438f75c80
tamateea <tamateea@gmail.com>
2022-12-19 15:38:51 +1300

    bump runelite-cache version to "1.9.6-SNAPSHOT"

--- qodat-api/build.gradle.kts
+++ qodat-api/build.gradle.kts
@@ -1,7 +1,7 @@
 version = "0.0.1"
 
 dependencies {
-    api(group = "net.runelite", name = "cache", version = "1.8.9") {
+    api(group = "net.runelite", name = "cache", version = "1.9.6-SNAPSHOT") {
         exclude(group = "com.google.common")
     }
 }
