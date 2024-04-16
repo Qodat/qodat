fa867472f8d307c297125adb0dce1def28640deb
stanvanderbend <stanvdbend@gmail.com>
2024-04-16 23:40:41 +0200

    Init AWT toolkit in main method to avoid mac os exception
    https://bugs.java.com/bugdatabase/view_bug?bug_id=8318129

--- src/main/kotlin/stan/qodat/Launcher.kt
+++ src/main/kotlin/stan/qodat/Launcher.kt
@@ -1,11 +1,13 @@
 package stan.qodat
 
+import java.awt.Toolkit
 import kotlin.time.ExperimentalTime
 
 object Launcher {
     @ExperimentalTime
     @JvmStatic
     fun main(args: Array<String>) {
-      Qodat.main(args)
+        Toolkit.getDefaultToolkit()
+        Qodat.main(args)
     }
 }
