bb10c6b95dceb9bdde49841a773ec043a1f3ae85
stanvanderbend <stanvdbend@gmail.com>
2024-07-02 22:34:11 +0200

    Updated runelite cache module for rev 223 support

--- qodat-api/build.gradle.kts
+++ qodat-api/build.gradle.kts
@@ -1,7 +1,7 @@
 version = "0.0.2"
 
 dependencies {
-    api(group = "net.runelite", name = "cache", version = "1.10.26.1") {
+    api(group = "net.runelite", name = "cache", version = "1.10.33") {
         exclude(group = "com.google.common")
     }
 }
