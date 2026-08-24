c189a13838cc1674b4bdf05a834bea924d2656ca
dosier <stanvdbend@gmail.com>
2026-08-25 00:04:51 +0200

    ci: checkout qodat-cache and avoid secrets in if
    Unit CI resolved an empty includeBuild because the submodule was
    not fetched. Workflow parse failed: secrets is not allowed in step if.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,9 @@ include("qodat-api")
 include("qodat-bench")
 //include("qodat-launcher")
 //include("qodat-launcher:buildSrc")
+require(file("qodat-cache/settings.gradle.kts").isFile) {
+    "qodat-cache submodule is not checked out. Run: git submodule update --init"
+}
 includeBuild("qodat-cache") {
     dependencySubstitution {
         substitute(module("com.displee:rs-cache-library")).using(project(":"))
